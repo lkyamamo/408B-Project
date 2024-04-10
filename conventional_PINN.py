61380b9204d1d5016a749d1fe92cafbf8c10c2bb
Logan Yamamoto <lkyamamo@usc.edu>
2024-04-09 18:37:08 -0700

    starting conventional PINN file and using tensorflow to create MLP. starting work on loss function definitions. Seeing if can use tf for that otherwise will use pytorch

--- conventional_PINN.py
+++ conventional_PINN.py
@@ -1,52 +1,41 @@
-import torch
-from torch import nn
+import tensorflow as tf
 
 #from Tetsuto Nagashima's implementation based off of AME 508 course work
-class MLP(nn.Module):
-    def __init__(self, input_dim=1, out_dim=1):
-        super(MLP, self).__init__()
+class AllenCahn():
+  def __init__(self, t, x, u0):
 
-        self.width = 200
-        self.depth = 4
+    #initial condition
+    self.u0 = u0
 
-        self.activation == nn.Tanh()
+    #time and spacial locations
+    self.t = t
+    self.x = x
 
-        # First hidden layer
-        MLP_list = [nn.Linear(input_dim, self.width)]
-
-        # Remaining hidden layers
-        for _ in range(self.depth - 1):
-            MLP_list.append(nn.Linear(self.width, self.width))
-
-        # Output layer
-        MLP_list.append(nn.Linear(self.width, out_dim))
-
-        # Adding list of layers as modules
-        self.model = nn.ModuleList(MLP_list)
-
-        def init_weights(layer):
-          if isinstance(layer, nn.Linear):
-              nn.init.normal_(layer.weight, mean=0, std=1)
-              if layer.bias is not None:
-                nn.init.normal_(layer.bias, mean=0, std=1)
-
-        self.model.apply(init_weights)
+    self.t0 = t[0]
+    self.tfinal = t[-1]
     
-    def forward(self, x):  # no activation in last layer (from A2)
-        for i, layer in enumerate(self.model):
-          if i < len(self.model) - 1:  # Skip the last layer
-            x = self.activation(layer(x))
-          else:
-            x = layer(x)  # No activatison in the final layer
+def get_model():
 
-#as defined by Raissi's paper
-def AC_loss_function_Raissi(x_train, t_train, model):
-    u = model(x_train)
-    u_x = torch.autograd.grad(u, x_train, grad_outputs=torch.ones_like(x_train), create_graph=True)[0]
-    u_xx = torch.autograd.grad(u_x, x_train, grad_outputs=torch.ones_like(x_train), create_graph=True)[0]
-    u_t = torch.autograd.grad(u, t_train, grad_outputs=torch.ones_like(t_train), create_graph=True)[0]
+  initializer = tf.keras.initalizers.GlorotUniform()
 
-def AC_loss_function_Wang(x_train, model):
+  inputs = tf.keras.Input(shape=(), name="Conventional_PINN")
+  x0 = tf.keras.layers.Flatten()(inputs)
+  x1 = tf.keras.layers.Dense(200, activation='tanh', kernel_initializer=initializer)(x0)
+  x2 = tf.keras.layers.Dense(200, activation='tanh', kernel_initializer=initializer)(x1)
+  x3 = tf.keras.layers.Dense(200, activation='tanh', kernel_initializer=initializer)(x2)
+  x4 = tf.keras.layers.Dense(200, activation='tanh', kernel_initializer=initializer)(x3)
+  outputs = tf.keras.layers.Dense(1, name="predictions")(x4)
+
+  model = tf.keras.Model(inputs=inputs, outputs=outputs, name='Model')
    
-        
-        
+  model.compile(
+      optimizer=tf.keras.optimizers.Adam(),
+      loss=AC_loss_function,
+      metrics=['accuracy']
+  )
+
+def AC_loss_function():
+  uic = 
+  Lic = tf.math.reduce_sum(tf.math.squared_difference(u(0,x), g))
+
+def get_matlab_data():
---
+++ main.py
@@ -0,0 +1,25 @@
+import scipy.io
+import os
+
+working_dir = os.getcwd()
+
+"""
+print("PINNs AC data")
+
+
+file_path = os.path.join(working_dir, 'PINNs', 'main', 'Data', 'AC.mat')
+
+data = scipy.io.loadmat(file_path)
+
+print(f"tt: {data['tt']}")
+print(f"uu: {data['uu']}")
+print(f"x: {data['x']}")
+
+"""
+print("jaxpi AC data")
+file_path = os.path.join(working_dir, 'jaxpi', 'examples', 'allen_cahn','data', 'allen_cahn.mat')
+
+data = scipy.io.loadmat(file_path)
+print(f"tt: {data['t']}")
+print(f"uu: {data['usol']}")
+print(f"x: {data['x']}")
