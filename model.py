6ae95c95e26851db4fae0e664648afe35fdfda71
Logan Yamamoto <lkyamamo@usc.edu>
2024-04-16 02:28:26 -0700

    finalized model according to training algorithm described in paper. Shown to converge to eroneous solution. Full analysis needs to be done

--- model.py
+++ model.py
@@ -22,45 +22,44 @@ class ConventionalAllenCahnPINN():
         print(f"usol: {usol.shape}")
 
         # initial condition inputs
-        self.x_initial = x.T
-        self.t_initial = np.array([0.]*len(x)).T
-        self.usol_initial = usol[0].T
+        self.x_initial = tf.cast(tf.convert_to_tensor(x.T), tf.float32)
+        self.t_initial = tf.cast(tf.convert_to_tensor(np.array([0.]*len(x)).T), tf.float32)
+        self.usol_initial = tf.cast(tf.convert_to_tensor(usol[0].T), tf.float32)
 
         print(f"t_initial: {self.t_initial.shape}")
         print(f"x_initial: {self.x_initial.shape}")
         print(f"usol_initial: {self.usol_initial.shape}")
 
-        self.t_mesh = []
-        self.x_mesh = []
+        self.t_train = []
+        self.x_train = []
         self.usol_train = []
 
         # restructure input data
         # shape of data (t_train, x_train, usol_train): (102912,0)
         for i in range(len(t)):
-            self.t_mesh += [t[i]]*(len(x))
-            self.x_mesh.append(x)
+            self.t_train += [t[i]]*(len(x))
+            self.x_train.append(x)
             self.usol_train.append(usol[i])
 
-        self.t_mesh = tf.cast(tf.convert_to_tensor(np.array(self.t_mesh)), tf.float32)
-        self.x_mesh = tf.cast(tf.convert_to_tensor(np.array(self.x_mesh).flatten()), tf.float32)
+        self.t_train = tf.cast(tf.convert_to_tensor(np.array(self.t_train)), tf.float32)
+        self.x_train = tf.cast(tf.convert_to_tensor(np.array(self.x_train).flatten()), tf.float32)
         self.usol_train = tf.cast(tf.convert_to_tensor(np.array(self.usol_train).flatten()), tf.float32)
-
-        self.x_train = []
-        self.t_train = []
 
         #np.save(file="t_train", arr=self.t_train)
         #np.save(file="x_train", arr=self.x_train)
         
-        print(f"x_mesh: {self.x_mesh.shape}")
-        print(f"t_mesh: {self.t_mesh.shape}")
+        print(f"x_train: {self.x_train.shape}")
+        print(f"t_train: {self.t_train.shape}")
         print(f"usol_train: {self.usol_train.shape}")
 
         # boundary condition inputs ((t,-1) and (t,1))
-        self.negative_bound = np.stack([t, np.array([-1]*len(t))]).T
-        self.positive_bound = np.stack([t, np.array([1]*len(t))]).T
+        self.x_negative_bound = tf.cast(tf.convert_to_tensor(np.array([-1]*len(t)).T), tf.float32)
+        self.x_positive_bound = tf.cast(tf.convert_to_tensor(np.array([1]*len(t)).T), tf.float32)
+        self.t_bc = tf.cast(tf.convert_to_tensor(np.array(t).T), tf.float32)
 
-        print(f"negative_bound: {self.negative_bound.shape}")
-        print(f"positive_bound: {self.positive_bound.shape}")
+        print(f"x_negative_bound: {self.x_negative_bound.shape}")
+        print(f"x_positive_bound: {self.x_positive_bound.shape}")
+        print(f"t_bc: {self.t_bc.shape}")
 
         # initialize model
         self.model = tf.keras.models.Sequential([
@@ -77,27 +76,45 @@ class ConventionalAllenCahnPINN():
         self.losses = {'total_loss':[], 'boundary_loss':[], 'residual_loss':[], 'initial_loss':[]}
 
 
-    def loss_function(self, u, t_tape, x_tape):
+    def loss_function(self):
         print("Calculating Loss Value")
+
+        # initial condition loss
+
+        u = self.model(tf.stack([self.t_initial, self.x_initial], axis=1))
+        loss_ic = tf.math.square(tf.math.reduce_mean((self.usol_initial - u)))
+
+        # boundary condition loss
+        with tf.GradientTape(persistent=True) as x_tape, tf.GradientTape(persistent=True) as t_tape:
+            t_tape.reset()
+            x_tape.reset()
+            x_tape.watch(self.x_positive_bound)
+            x_tape.watch(self.x_negative_bound)
+
+            u_pos = self.model(tf.stack([self.t_bc, self.x_positive_bound], axis=1))
+            u_neg = self.model(tf.stack([self.t_bc, self.x_negative_bound], axis=1))
+
+        u_pos_x = tf.expand_dims(x_tape.gradient(u, self.x_positive_bound, unconnected_gradients=tf.UnconnectedGradients.ZERO), axis=-1)
+        u_neg_x = tf.expand_dims(x_tape.gradient(u, self.x_negative_bound, unconnected_gradients=tf.UnconnectedGradients.ZERO), axis=-1)
+
+        loss_bc = tf.math.square(tf.math.reduce_mean((u_pos - u_neg))) + tf.math.square(tf.math.reduce_mean((u_pos_x - u_neg_x)))
+
+        # residual loss 
+        with tf.GradientTape(persistent=True) as x_tape, tf.GradientTape(persistent=True) as t_tape:
+            t_tape.reset()
+            x_tape.reset()
+            t_tape.watch(self.t_train)
+            x_tape.watch(self.x_train)
+
+            u = self.model(tf.stack([self.t_train, self.x_train], axis=1))
+
         u_t = tf.expand_dims(t_tape.gradient(u, self.t_train, unconnected_gradients=tf.UnconnectedGradients.ZERO), axis=-1)
         u_x = tf.expand_dims(x_tape.gradient(u, self.x_train, unconnected_gradients=tf.UnconnectedGradients.ZERO), axis=-1)
         u_xx = tf.expand_dims(x_tape.gradient(u_x, self.x_train, unconnected_gradients=tf.UnconnectedGradients.ZERO), axis=-1)
-
-        # residual loss 
+        
         loss_r = tf.math.square(tf.math.reduce_mean(u_t - 0.0001 * u_xx + 5 * tf.math.pow(u_xx,[3]) - 5*u))
 
-        # initial condition loss
-        loss_ic = tf.math.square(tf.math.reduce_mean((self.usol_initial - self.model(tf.stack(self.t_initial, self.x_intial)))))
-
-        # boundary condition loss
-        loss_bc = tf.math.square(tf.math.reduce_mean((u[0::512] - u[511:512]))) + tf.math.square(tf.math.reduce_mean((u_x[0::512] - u_x[511::512])))
-
         total_loss = self.lambda_r*loss_r + self.lambda_ic*loss_ic + self.lambda_bc*loss_bc
-
-        print(total_loss)
-        print(loss_r)
-        print(loss_ic)
-        print(loss_bc)
 
         self.losses['total_loss'].append(total_loss.numpy().item())
         self.losses['residual_loss'].append(loss_r.numpy().item())
@@ -112,24 +129,11 @@ class ConventionalAllenCahnPINN():
         for epoch in range(max_epochs):
             print(f"Epoch {epoch + 1}")
 
-            self.x_train = tf.random.shuffle(self.x_mesh)[:self.batch_size]
-            self.t_train = tf.random.shuffle(self.t_mesh)[:self.batch_size]
-
             with tf.GradientTape(persistent=True) as loss_tape:
                 loss_tape.reset()
                 loss_tape.watch(self.model.trainable_variables)
-                with tf.GradientTape(persistent=True) as x_tape, tf.GradientTape(persistent=True) as t_tape:
-                    x_tape.reset()
-                    t_tape.reset()
-
-                    t_tape.watch(self.t_train)
-                    x_tape.watch(self.x_train)
-
-                    inputs = tf.stack([self.t_train,self.x_train],axis=1)
                 
-                    u = self.model(inputs)
-
-                total_loss, loss_r, loss_ic, loss_bc = self.loss_function(u, t_tape, x_tape)
+                total_loss, loss_r, loss_ic, loss_bc = self.loss_function()
 
             print("Updating Weights")
 
@@ -143,16 +147,16 @@ class ConventionalAllenCahnPINN():
                 bc_grad_norm = self.compute_L2_norm(bc_grad)
                 r_grad_norm = self.compute_L2_norm(r_grad)
 
-                self.lambda_ic = (ic_grad_norm + bc_grad_norm + r_grad_norm)/ic_grad_norm
-                self.lambda_bc = (ic_grad_norm + bc_grad_norm + r_grad_norm)/bc_grad_norm
-                self.lambda_r = (ic_grad_norm + bc_grad_norm + r_grad_norm)/r_grad_norm
+                self.lambda_ic = self.alpha*self.lambda_ic + (1-self.alpha)*(ic_grad_norm + bc_grad_norm + r_grad_norm)/ic_grad_norm
+                self.lambda_bc = self.alpha*self.lambda_bc + (1-self.alpha)*(ic_grad_norm + bc_grad_norm + r_grad_norm)/bc_grad_norm
+                self.lambda_r = self.alpha*self.lambda_r + (1-self.alpha)*(ic_grad_norm + bc_grad_norm + r_grad_norm)/r_grad_norm
 
             total_grad = loss_tape.gradient(total_loss, self.model.trainable_variables, unconnected_gradients=tf.UnconnectedGradients.ZERO)
 
             optimizer.apply_gradients(zip(total_grad, self.model.trainable_variables))
 
     def evaluate(self):
-        inputs = tf.stack([self.t_mesh,self.x_mesh],axis=1)
+        inputs = tf.stack([self.t_train,self.x_train],axis=1)
         output = self.model(inputs)
         
         return tf.reshape(output,(201,512))
